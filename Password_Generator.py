b117b808f29d8851979ba273e179ff64f608e038
George <appeahg@outlook.com>
2023-11-20 20:33:20 +0000

    Added new features and deleted file

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,10 +1,41 @@
-print("hello world")
+import random
+import string
 
-
-dcdsocndkcdnccldsn
-cdscndsclnczdl;c 
-creditszsdsl;c dnl d 
-zdc zd
- dzpcmzd;'cmdsc';dscdl;cndsl 
- creditscspcmsdc;dsmcsdc mdl; dsc[]dscsd
- 
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special_characters = string.punctuation
+    
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special_characters
+        
+    pwrd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+    
+    while not meets_criteria or len(pwrd) < min_length:
+        new_char = random.choice(characters)
+        pwrd += new_char
+        
+        if new_char in digits:
+            has_number = True
+        elif new_char in special_characters:
+            has_special = True
+            
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+    
+    return pwrd
+        
+min_length = int(input("Minimum password length: "))
+has_numbers = input("Need numbers? (y/n): ").lower() == "y"
+has_special = input("Need special characters? (y/n): ").lower() == "y"
+pwrd = generate_password(min_length, has_numbers, has_special)
+print("Generated password: ", pwrd)
